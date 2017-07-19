58d45ae88fd3794cfe79db3dbd14d2236c1b5f9d
shane0531 <tjdghks0531@gmail.com>
2017-07-19 11:15:27 +0900

    Update kafka_to_db.js

--- SmartX-mini/ubuntu-kafkatodb/kafka_to_db.js
+++ SmartX-mini/ubuntu-kafkatodb/kafka_to_db.js
@@ -8,9 +8,9 @@ var DB = new influx.InfluxDB({
     host: 'nuc',
     port: 8086, // optional, default 8086
     protocol: 'http', // optional, default 'http'
-    username: 'admin',
-    password: 'admin',
-    database: 'labs'
+    username: 'dnslabInflux',
+    password: 'dnslab',
+    database: 'dnslabdatabases'
 });
 
 var resourceKafka = new kafka.Client('nuc:2181');
@@ -18,17 +18,39 @@ var resourceOffset = new kafka.Offset(resourceKafka);
 
 console.log('fetch');
 resourceOffset.fetch([{
-        topic: 'test',
+        topic: 'sensor',
         partition: 0,
+        time: -1,
+        maxNum: 1
+	},
+	{
+        topic: 'sensor',
+        partition: 1,
+        time: -1,
+        maxNum: 1
+    },
+    {
+        topic: 'sensor',
+        partition: 2,
         time: -1,
         maxNum: 1
     }
 ], function(err, data) {
 	console.log(data);
     var resourceConsumer = new kafka.Consumer(resourceKafka, [{
-            topic: 'test',
+            topic: 'sensor',
             partition: 0,
-            offset: data['test'][0]
+            offset: data['sensor'][0]
+        },
+	{
+            topic: 'sensor',
+            partition: 1,
+            offset: data['sensor'][0]
+        },
+	{
+            topic: 'sensor',
+            partition: 2,
+            offset: data['sensor'][0]
         }
     ], {
         autoCommit: false,
@@ -37,14 +59,14 @@ resourceOffset.fetch([{
 
     resourceConsumer.on('message', function(message) {
         var messageJSON = JSON.parse(message.value);
-	console.log(messageJSON);
 
         DB.writePoints([{
-            measurement: 'test',
+            measurement: 'sensor',
             tags: {
             },
             fields: {
-          	light: messageJSON.light
+          	light: messageJSON.light,
+		temp: messageJSON.temp
             },
         }])
 
