8c7d42f5f9264b9d5d1cb89c6bba610cd569eb4b
shane0531 <tjdghks0531@gmail.com>
2017-09-20 01:28:22 -0400

    final

--- SmartX-mini/ubuntu-kafkatodb/kafka_to_db.js
+++ SmartX-mini/ubuntu-kafkatodb/kafka_to_db.js
@@ -6,15 +6,15 @@ console.log('start');
 // InfluxDB
 var DB = new influx.InfluxDB({
     // single-host configuration
-    host: 'nuc',
+    host: '210.114.90.176',
     port: 8086, // optional, default 8086
     protocol: 'http', // optional, default 'http'
-    username: 'dnslabInflux',
-    password: 'dnslab',
-    database: 'dnslabdatabases'
+    username: 'id',
+    password: 'password',
+    database: 'station'
 });
 
-var resourceKafka = new kafka.Client('nuc:2181');
+var resourceKafka = new kafka.Client('210.114.90.176:2181');
 var resourceOffset = new kafka.Offset(resourceKafka);
 
 console.log('fetch');
@@ -47,12 +47,10 @@ resourceOffset.fetch([{
           	light: messageJSON.light,
 		temp: messageJSON.temp,
 		latitude: messageJSON.latitude,
-		logitude: messageJSON.logitude,
-		ratio: messageJSON.ratio,
-		concentration: messageJSON.concentration,
-		lowpulseoccupancy: messageJSON.lowpulseoccupancy,
-		humidity: messageJSON.humidity,
-		luminance: messageJSON.luminance
+		longitude: messageJSON.longitude,
+		dust: messageJSON.dust,
+		alcohol_gas: messageJSON.alcohol_gas,
+		co_gas: messageJSON.co_gas
             },
         }])
         console.log(messageJSON);
